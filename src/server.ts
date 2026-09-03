29cbef121f08b45e3af73bb1465cc6208262a540
Sebastian09-cell <sebastiantorresdelgado526o@gmail.com>
2026-09-03 01:19:09 -0500

    Preparar para deploy

--- src/server.ts
+++ src/server.ts
@@ -6,4 +6,5 @@ app.use(express.json());
 app.use(cors());
 app.use("/libros", librosRoutes);
 
-app.listen(3000, () => console.log("Servidor en puerto 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor en puerto ${PORT}`));
